7cddd5dc13b346964073ac6991c9f91451341dfe
Sergey Dolin <sergey@s4y.solutions>
2018-10-03 22:25:19 +0500

    Detect BluetoothManager presense and select framgent accordinly

--- app/src/main/java/solutions/s4y/itag/MainActivity.java
+++ app/src/main/java/solutions/s4y/itag/MainActivity.java
@@ -1,13 +1,19 @@
 package solutions.s4y.itag;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+    static private final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +25,23 @@ public class MainActivity extends Activity {
         String appLinkAction = appLinkIntent.getAction();
         Uri appLinkData = appLinkIntent.getData();
         */
-
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        if (bluetoothManager != null)
+            mBluetoothAdapter = bluetoothManager.getAdapter();
     }
 
     private void setupContent() {
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        NoBLEFragment fragment = new NoBLEFragment();
+        final FragmentManager fragmentManager = getFragmentManager();
+        final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment fragment;
+        if (mBluetoothAdapter==null) {
+            fragment = new NoBLEFragment();
+        }else {
+            fragment = new BLEFragment();
+        }
         fragmentTransaction.add(R.id.content, fragment);
         fragmentTransaction.commit();
-
     }
 
     @Override
@@ -36,4 +49,5 @@ public class MainActivity extends Activity {
         super.onResume();
         setupContent();
     }
+
 }
