aaa64b67e9f7431e3f79dfefc1a7c54dee687774
Sergey Dolin <sergey@s4y.solutions>
2018-10-05 02:05:20 +0500

    migrate test from com.android.support to androidx

--- app/src/androidTest/java/solutions/s4y/itag/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/solutions/s4y/itag/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
 package solutions.s4y.itag;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
