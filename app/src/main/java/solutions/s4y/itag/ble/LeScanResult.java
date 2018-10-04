f07098b8f754114d6f3d8d0993b40e9320d4a980
Sergey Dolin <sergey@s4y.solutions>
2018-10-04 21:37:58 +0500

    Add LeScanResult class to hold the results of LeScan

---
+++ app/src/main/java/solutions/s4y/itag/ble/LeScanResult.java
@@ -0,0 +1,15 @@
+package solutions.s4y.itag.ble;
+
+import android.bluetooth.BluetoothDevice;
+
+public class LeScanResult {
+    final BluetoothDevice device;
+    final int rssi;
+    final byte[] scanRecord;
+
+    public LeScanResult(BluetoothDevice device, int rssi, byte[] scanRecord) {
+        this.device = device;
+        this.rssi = rssi;
+        this.scanRecord = scanRecord;
+    }
+}
