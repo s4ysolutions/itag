a8e119b2c65ac284a5ef7546fa5a548ceba6dd16
Sergey Dolin <sergey@s4y.solutions>
2018-10-04 21:35:41 +0500

    Add error notifier rxjava subject

---
+++ app/src/main/java/solutions/s4y/itag/ITagApplication.java
@@ -0,0 +1,18 @@
+package solutions.s4y.itag;
+
+import android.app.Application;
+import android.widget.Toast;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.subjects.PublishSubject;
+
+public final class ITagApplication extends Application {
+    final static public PublishSubject<Exception> errorNotifier = PublishSubject.create();
+    final private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mCompositeDisposable.add(errorNotifier.subscribe(ex-> Toast.makeText(this, ex.getMessage(),Toast.LENGTH_LONG).show()));
+    }
+}
