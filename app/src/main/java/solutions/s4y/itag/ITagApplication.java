eec145610be5a634088f30edb6ee1ff81a9d4e3e
Sergey Dolin <sergey@s4y.solutions>
2018-10-07 02:49:20 +0500

    load database on startup

--- app/src/main/java/solutions/s4y/itag/ITagApplication.java
+++ app/src/main/java/solutions/s4y/itag/ITagApplication.java
@@ -5,6 +5,7 @@ import android.widget.Toast;
 
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.subjects.PublishSubject;
+import solutions.s4y.itag.ble.Db;
 
 public final class ITagApplication extends Application {
     final static public PublishSubject<Throwable> errorNotifier = PublishSubject.create();
@@ -13,6 +14,7 @@ public final class ITagApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Db.load(this);
         mCompositeDisposable.add(errorNotifier.subscribe(ex-> Toast.makeText(this, ex.getMessage(),Toast.LENGTH_LONG).show()));
     }
 }
