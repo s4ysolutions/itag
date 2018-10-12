b59beb4115a5c55141365da44abefd80df17d8cd
Sergey Dolin <sergey@s4y.solutions>
2018-10-12 17:02:03 +0500

    refactor error notifier to do not use rx*

--- app/src/main/java/solutions/s4y/itag/ITagApplication.java
+++ app/src/main/java/solutions/s4y/itag/ITagApplication.java
@@ -1,31 +1,35 @@
 package solutions.s4y.itag;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.widget.Toast;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subjects.PublishSubject;
 import solutions.s4y.itag.ble.Db;
 
 public final class ITagApplication extends Application {
-    final static public PublishSubject<Throwable> errorNotifier = PublishSubject.create();
-    final private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
+    private final static String LT = ITagApplication.class.getName();
+    @SuppressLint("StaticFieldLeak")
+    private static Context context;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        context=this;
         Db.load(this);
-        mCompositeDisposable.add(errorNotifier
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        ex ->
-                                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show(),
-                        ex ->
-                                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show(),
-                        () -> {
-                        }
-                ));
     }
+
+    static public void handleError(Throwable th) {
+        new Handler(Looper.getMainLooper()).post(() -> {
+            if (context==null) {
+                Log.e(LT,"Attempt to handle error before application created", th);
+            }else {
+                Toast.makeText(context, th.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 }
