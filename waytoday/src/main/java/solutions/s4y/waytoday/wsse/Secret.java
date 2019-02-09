d3833e7ff74f46ca3ffc1db1f03c4fd7d663063c
Sergey Dolin <sergey@s4y.solutions>
2019-02-09 14:15:24 +0500

    Add wsse

---
+++ waytoday/src/main/java/solutions/s4y/waytoday/wsse/Secret.java
@@ -0,0 +1,10 @@
+package solutions.s4y.waytoday.wsse;
+
+import androidx.annotation.NonNull;
+
+public class Secret {
+    @NonNull
+    static String get() {
+        return "androidx..NonNull";
+    }
+}
