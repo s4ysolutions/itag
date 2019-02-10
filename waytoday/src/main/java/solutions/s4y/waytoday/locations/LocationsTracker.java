313c8d05fc34f4eb7d0637a899a9015359e903ed
Sergey Dolin <sergey@s4y.solutions>
2019-02-10 13:37:43 +0500

    Add listeners to LocationsTracker

--- waytoday/src/main/java/solutions/s4y/waytoday/locations/LocationsTracker.java
+++ waytoday/src/main/java/solutions/s4y/waytoday/locations/LocationsTracker.java
@@ -9,15 +9,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
 import solutions.s4y.waytoday.BuildConfig;
-import solutions.s4y.waytoday.errors.ErrorsObservable;
 import solutions.s4y.waytoday.grpc.LocationOuterClass;
 
 public class LocationsTracker {
     private static final List<ILocationListener> mLocationListeners =
             new ArrayList<>(2);
     private static final String LT = LocationOuterClass.Location.class.getSimpleName();
-    public static boolean isSuspended;
+    @VisibleForTesting
+    private static final List<ITrackingStateListener> sStateListeners =
+            new ArrayList<>(2);
+    private static boolean isSuspended;
     static private final LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
@@ -29,9 +32,6 @@ public class LocationsTracker {
 
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
-            ErrorsObservable.notify(
-                    "LocationListener.onStatusChanged: " + status,
-                    BuildConfig.DEBUG);
         }
 
         @Override
@@ -40,6 +40,7 @@ public class LocationsTracker {
                 Log.d(LT, "onProviderEnabled");
             }
             isSuspended = false;
+            notifyStateChange();
         }
 
         @Override
@@ -48,6 +49,7 @@ public class LocationsTracker {
                 Log.d(LT, "onProviderDisabled");
             }
             isSuspended = true;
+            notifyStateChange();
         }
     };
     static private final RequestUpdatesListener requestListener = new RequestUpdatesListener() {
@@ -57,23 +59,24 @@ public class LocationsTracker {
                 Log.d(LT, "onRequestResult: " + success);
             }
             isSuspended = !success;
+            notifyStateChange();
         }
     };
-    public static boolean isUpdating;
     private static LocationsUpdater updater;
+    private static boolean isUpdating;
 
-    public static void addOnITagChangeListener(ILocationListener listener) {
+    public static void addOnILocationListener(ILocationListener listener) {
         mLocationListeners.add(listener);
-    }
-
-    public static void removeOnITagChangeListener(ILocationListener listener) {
-        mLocationListeners.remove(listener);
     }
 
     private static void notifyLocation(@NonNull Location location) {
         for (ILocationListener listener : mLocationListeners) {
             listener.onLocation(location);
         }
+    }
+
+    public static void removeOnILocationListener(ILocationListener listener) {
+        mLocationListeners.remove(listener);
     }
 
     public static void requestStart(@NonNull final LocationsUpdater updater, int frequency) {
@@ -84,7 +87,12 @@ public class LocationsTracker {
         LocationsTracker.updater = updater;
         isUpdating = true;
         isSuspended = false;
+        notifyStateChange();
         updater.requestLocationUpdates(locationListener, requestListener, frequency);
+    }
+
+    public interface ILocationListener {
+        void onLocation(@NonNull Location location);
     }
 
     public static void stop() {
@@ -97,16 +105,32 @@ public class LocationsTracker {
         }
         if (isUpdating) {
             isUpdating = false;
+            notifyStateChange();
         }
     }
 
-    public interface ILocationListener {
-        void onLocation(@NonNull Location location);
+    public static void addOnTrackingStateListener(ITrackingStateListener listener) {
+        sStateListeners.add(listener);
+    }
+
+    public static void removeOnTrackingStateListener(ITrackingStateListener listener) {
+        sStateListeners.remove(listener);
+    }
+
+    private static void notifyStateChange() {
+        TrackingState state = new TrackingState();
+        for (ITrackingStateListener listener : sStateListeners) {
+            listener.onStateChange(state);
+        }
+    }
+
+    public interface ITrackingStateListener {
+        void onStateChange(@NonNull TrackingState state);
     }
 
     static public class TrackingState {
-        final boolean isUpdating;
-        final boolean isSuspended;
+        public final boolean isUpdating;
+        public final boolean isSuspended;
 
         TrackingState() {
             this.isUpdating = LocationsTracker.isUpdating;
