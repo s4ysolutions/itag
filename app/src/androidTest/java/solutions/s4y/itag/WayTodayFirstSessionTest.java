c18ddf5c79ae5590a3e0fe8d0879dde62f1ad197
Sergey Dolin <sergey@s4y.solutions>
2019-02-10 13:47:01 +0500

    Add integration tests

---
+++ app/src/androidTest/java/solutions/s4y/itag/WayTodayFirstSessionTest.java
@@ -0,0 +1,119 @@
+package solutions.s4y.itag;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.util.List;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.ViewInteraction;
+import solutions.s4y.waytoday.idservice.IDService;
+import solutions.s4y.waytoday.locations.LocationsTracker;
+import solutions.s4y.waytoday.upload.UploadJobService;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+
+public class WayTodayFirstSessionTest {
+    private static IDService.IIDSeriviceListener mockIDListener =
+            mock(IDService.IIDSeriviceListener.class);
+    private static UploadJobService.IUploadListener mockUploadListener =
+            mock(UploadJobService.IUploadListener.class);
+    private static LocationsTracker.ITrackingStateListener mockTrackingStateListener
+            = mock(LocationsTracker.ITrackingStateListener.class);
+    private static LocationsTracker.ILocationListener mockLocationListener
+            = mock(LocationsTracker.ILocationListener.class);
+
+    @BeforeClass
+    public static void beforeClass() {
+        IDService.addOnITagChangeListener(mockIDListener);
+        UploadJobService.addOnITagChangeListener(mockUploadListener);
+        LocationsTracker.addOnTrackingStateListener(mockTrackingStateListener);
+        LocationsTracker.addOnILocationListener(mockLocationListener);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        IDService.removeOnITagChangeListener(mockIDListener);
+        UploadJobService.removeOnITagChangeListener(mockUploadListener);
+        LocationsTracker.removeOnTrackingStateListener(mockTrackingStateListener);
+        LocationsTracker.removeOnILocationListener(mockLocationListener);
+    }
+
+    @Before
+    public void before() {
+        Application application = ApplicationProvider.getApplicationContext();
+        SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(application);
+        p.edit().remove("freq").apply();
+        p.edit().remove("tid").apply();
+        p.edit().remove("wt").apply();
+        reset(mockIDListener);
+        reset(mockUploadListener);
+    }
+
+    @After
+    public void after() {
+    }
+
+    @Test
+    public void activity_shouldHaveWayTodayMenu() {
+
+        try (ActivityScenario<MainActivity> ignored = ActivityScenario.launch(MainActivity.class)) {
+
+            ViewInteraction vi = onView(withId(R.id.btn_waytoday));
+            vi.perform(click());
+            onView(withText(R.string.off))
+                    .perform(click());
+        }
+    }
+
+    @Test
+    public void activity_firstLaunchWT() {
+        try (ActivityScenario<MainActivity> ignored = ActivityScenario.launch(MainActivity.class)) {
+            // open waytoday menu
+            onView(withId(R.id.btn_waytoday))
+                    .perform(click());
+            //close waytoday menu
+            onView(withText(R.string.freq_min_1))
+                    .perform(click());
+
+            Application application = ApplicationProvider.getApplicationContext();
+            SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(application);
+            assertThat(p.getInt("freq", -1), equalTo(60000));
+            assertThat(p.getBoolean("wt", false), is(true));
+
+            ArgumentCaptor<LocationsTracker.TrackingState> state =
+                    ArgumentCaptor.forClass(LocationsTracker.TrackingState.class);
+
+            verify(mockIDListener, timeout(10000)).onTrackID(any());
+            verify(mockTrackingStateListener, timeout(1000).times(2)).onStateChange(state.capture());
+            List<LocationsTracker.TrackingState> states =
+                    state.getAllValues();
+            assertThat(states.size(), is(2));
+            assertThat(states.get(0).isSuspended, is(false));
+            assertThat(states.get(0).isUpdating, is(true));
+            assertThat(states.get(1).isSuspended, is(anything()));
+            assertThat(states.get(1).isUpdating, is(true));
+        }
+    }
+}
---
+++ app/src/androidTest/java/solutions/s4y/itag/WaytodayServicesTest.java
@@ -0,0 +1,86 @@
+package solutions.s4y.itag;
+
+import android.location.Location;
+import android.location.LocationManager;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+
+import java.util.List;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import solutions.s4y.waytoday.idservice.IDService;
+import solutions.s4y.waytoday.upload.UploadJobService;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class WaytodayServicesTest {
+    static IDService.IIDSeriviceListener mockIDListiner = mock(IDService.IIDSeriviceListener.class);
+    static UploadJobService.IUploadListener mockUploadListiner = mock(UploadJobService.IUploadListener.class);
+
+    @BeforeClass
+    public static void beforeClass() {
+        IDService.addOnITagChangeListener(mockIDListiner);
+        UploadJobService.addOnITagChangeListener(mockUploadListiner);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        IDService.removeOnITagChangeListener(mockIDListiner);
+        UploadJobService.removeOnITagChangeListener(mockUploadListiner);
+    }
+
+    @Before
+    public void before() {
+        reset(mockIDListiner);
+        reset(mockUploadListiner);
+    }
+
+    @After
+    public void after() {
+    }
+
+
+    @Test
+    public void idService_shouldReturnTrackID() {
+        assertThat(IDService.sListeners.size(), equalTo(1));
+        verify(mockIDListiner, never()).onTrackID(any());
+
+        IDService.enqueueRetrieveId(ApplicationProvider.getApplicationContext(), "");
+        verify(mockIDListiner, timeout(5000).times(1)).onTrackID(any());
+    }
+
+    @Test
+    public void uplaodService_shouldUploadLocation() {
+        ArgumentCaptor<UploadJobService.Status> argument =
+                ArgumentCaptor.forClass(UploadJobService.Status.class);
+        Location location = new Location(LocationManager.GPS_PROVIDER);
+        UploadJobService.enqueueUploadLocation(
+                ApplicationProvider.getApplicationContext(),
+                location
+        );
+        verify(mockUploadListiner, timeout(5000).times(3)).onStatusChange(argument.capture());
+        List<UploadJobService.Status> statuses = argument.getAllValues();
+        assertThat(statuses.size(), equalTo(3));
+        assertThat(statuses.get(0), equalTo(UploadJobService.Status.QUEUED));
+        assertThat(statuses.get(1), equalTo(UploadJobService.Status.UPLOADING));
+        assertThat(statuses.get(2), equalTo(UploadJobService.Status.EMPTY));
+    }
+
+}
