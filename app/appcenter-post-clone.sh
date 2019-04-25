b49dcafd3b3a4b33de75713500f3dedf85a35199
Sergey Dolin <sergey@s4y.solutions>
2019-04-25 11:59:26 +0500

    Fix waytoday secret class path

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo $GS |sed 's/\\"/"/g' > "$APPCENTER_SOURCE_DIRECTORY/app/google-services.json"
 
-cat > "$APPCENTER_SOURCE_DIRECTORY/app/src/main/java/s4y/waytoday/wsse/Secret.java" <<EOT
+cat > "$APPCENTER_SOURCE_DIRECTORY/waytoday/src/main/java/s4y/waytoday/wsse/Secret.java" <<EOT
 package s4y.waytoday.wsse;
 
 import androidx.annotation.NonNull;
