84cfa29ff8c0fc06967153325d34d3f61206b42a
Sergey Dolin <sergey@s4y.solutions>
2020-09-24 08:27:25 +0500

    Add kalman filter

---
+++ waytoday/src/main/java/mad/location/manager/lib/Commons/Coordinates.java
@@ -0,0 +1,90 @@
+package mad.location.manager.lib.Commons;
+
+/**
+ * Created by lezh1k on 2/13/18.
+ */
+
+public class Coordinates {
+    private static final double EARTH_RADIUS = 6371.0 * 1000.0; // meters
+
+    public static double distanceBetween(double lon1, double lat1, double lon2, double lat2) {
+        double deltaLon = Math.toRadians(lon2 - lon1);
+        double deltaLat = Math.toRadians(lat2 - lat1);
+        double a =
+                Math.pow(Math.sin(deltaLat / 2.0), 2.0) +
+                        Math.cos(Math.toRadians(lat1)) *
+                                Math.cos(Math.toRadians(lat2)) *
+                                Math.pow(Math.sin(deltaLon/2.0), 2.0);
+        double c = 2.0 * Math.atan2(Math.sqrt(a), Math.sqrt(1.0-a));
+        return EARTH_RADIUS * c;
+    }
+
+    public static double longitudeToMeters(double lon) {
+        double distance = distanceBetween(lon, 0.0, 0.0, 0.0);
+        return distance * (lon < 0.0 ? -1.0 : 1.0);
+    }
+
+    public static GeoPoint metersToGeoPoint(double lonMeters,
+                                            double latMeters) {
+        GeoPoint point = new GeoPoint(0.0, 0.0);
+        GeoPoint pointEast = pointPlusDistanceEast(point, lonMeters);
+        GeoPoint pointNorthEast = pointPlusDistanceNorth(pointEast, latMeters);
+        return pointNorthEast;
+    }
+
+    public static double latitudeToMeters(double lat) {
+        double distance = distanceBetween(0.0, lat, 0.0, 0.0);
+        return distance * (lat < 0.0 ? -1.0 : 1.0);
+    }
+
+    private static GeoPoint getPointAhead(GeoPoint point,
+                                          double distance,
+                                          double azimuthDegrees) {
+
+        double radiusFraction = distance / EARTH_RADIUS;
+        double bearing = Math.toRadians(azimuthDegrees);
+        double lat1 = Math.toRadians(point.Latitude);
+        double lng1 = Math.toRadians(point.Longitude);
+
+        double lat2_part1 = Math.sin(lat1) * Math.cos(radiusFraction);
+        double lat2_part2 = Math.cos(lat1) * Math.sin(radiusFraction) * Math.cos(bearing);
+        double lat2 = Math.asin(lat2_part1 + lat2_part2);
+
+        double lng2_part1 = Math.sin(bearing) * Math.sin(radiusFraction) * Math.cos(lat1);
+        double lng2_part2 = Math.cos(radiusFraction) - Math.sin(lat1) * Math.sin(lat2);
+        double lng2 = lng1 + Math.atan2(lng2_part1, lng2_part2);
+
+        lng2 = (lng2 + 3.0* Math.PI) % (2.0* Math.PI) - Math.PI;
+
+        GeoPoint res = new GeoPoint(Math.toDegrees(lat2), Math.toDegrees(lng2));
+        return res;
+    }
+
+    private static GeoPoint pointPlusDistanceEast(GeoPoint point, double distance) {
+        return getPointAhead(point, distance, 90.0);
+    }
+
+    private static GeoPoint pointPlusDistanceNorth(GeoPoint point, double distance) {
+        return getPointAhead(point, distance, 0.0);
+    }
+
+    public static double calculateDistance(GeoPoint[] track) {
+        double distance = 0.0;
+        double lastLon, lastLat;
+        //WARNING! I didn't find array.length type. Seems it's int, so we can use next comparison:
+        if (track == null || track.length - 1 <= 0) //track.length == 0 || track.length == 1
+            return 0.0;
+
+        lastLon = track[0].Longitude;
+        lastLat = track[0].Latitude;
+
+        for (int i = 1; i < track.length; ++i) {
+            distance += Coordinates.distanceBetween(
+                    lastLat, lastLon,
+                    track[i].Latitude, track[i].Longitude);
+            lastLat = track[i].Latitude;
+            lastLon = track[i].Longitude;
+        }
+        return distance;
+    }
+}
--- waytoday/src/main/java/s4y/waytoday/locations/LocationsTracker.java
+++ waytoday/src/main/java/s4y/waytoday/locations/LocationsTracker.java
@@ -5,11 +5,16 @@ import android.location.LocationListener;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.VisibleForTesting;
+import mad.location.manager.lib.Commons.Coordinates;
+import mad.location.manager.lib.Commons.GeoPoint;
+import mad.location.manager.lib.Commons.Utils;
+import mad.location.manager.lib.Filters.GPSAccKalmanFilter;
 import s4y.waytoday.BuildConfig;
 import s4y.waytoday.grpc.LocationOuterClass;
 
@@ -22,12 +27,139 @@ public class LocationsTracker {
             new ArrayList<>(2);
     @SuppressWarnings("WeakerAccess")
     public static boolean isSuspended;
+    public static final DataItemAcc zero = new DataItemAcc(0, 0, 0);
+    private static GPSAccKalmanFilter mKalmanFilter;
+    private static FilterSettings filterSettings = FilterSettings.defaultSettings;
+    private static DataItemGPS lastDataItemGPS = new DataItemGPS(null);
+    private static float minDistance = 1;
+
     static private final LocationListener locationListener = new LocationListener() {
+
+        double lastGPSTimeStamp = 0;
+        double prevLat = 0;
+        double prevLon = 0;
+
+        private void handlePredict(DataItemAcc acc) {
+            DataItemGPS gps = lastDataItemGPS;
+            if (gps.location == null) {
+                if (BuildConfig.DEBUG) {
+                    Log.d(LT, "kalmanFilter.predict (no location): east=" + acc.absEastAcc + " north=" + acc.absNorthAcc);
+                }
+                mKalmanFilter.predict(acc.getTimestamp(), acc.absEastAcc, acc.absNorthAcc);
+            } else {
+                double declination = gps.getDeclination();
+                if (BuildConfig.DEBUG) {
+                    Log.d(LT, "kalmanFilter.predict: (location): east=" + acc.getAbsEastAcc(declination) + " north=" + acc.getAbsNorthAcc(declination) + " decl=" + declination);
+                }
+                mKalmanFilter.predict(acc.getTimestamp(), acc.getAbsEastAcc(declination), acc.getAbsNorthAcc(declination));
+            }
+        }
+
+        private void handleUpdate(DataItemGPS gps, Location location) {
+            double xVel = location.getSpeed() * Math.cos(location.getBearing());
+            double yVel = location.getSpeed() * Math.sin(location.getBearing());
+
+            if (BuildConfig.DEBUG) {
+                if (gps.location != null) {
+                    Log.d(LT, "kalmanFilter.update: lon=" + gps.location.getLongitude() + " lat=" + gps.location.getLatitude() + " xVel=" + xVel + " yVel=" + yVel);
+                }
+            }
+
+            mKalmanFilter.update(
+                    gps.getTimestamp(),
+                    Coordinates.longitudeToMeters(location.getLongitude()),
+                    Coordinates.latitudeToMeters(location.getLatitude()),
+                    xVel,
+                    yVel,
+                    location.getAccuracy(),
+                    gps.getVelErr()
+            );
+        }
+
+        private Location locationAfterUpdateStep(Location location) {
+            double xVel, yVel;
+            Location loc = new Location("WayTodayAnroid");
+            GeoPoint pp = Coordinates.metersToGeoPoint(mKalmanFilter.getCurrentX(),
+                    mKalmanFilter.getCurrentY());
+            loc.setLatitude(pp.Latitude);
+            loc.setLongitude(pp.Longitude);
+            loc.setAltitude(location.getAltitude());
+            xVel = mKalmanFilter.getCurrentXVel();
+            yVel = mKalmanFilter.getCurrentYVel();
+            double speed = Math.sqrt(xVel * xVel + yVel * yVel); //scalar speed without bearing
+            loc.setBearing(location.getBearing());
+            loc.setSpeed((float) speed);
+            loc.setTime(System.currentTimeMillis());
+            loc.setElapsedRealtimeNanos(System.nanoTime());
+            loc.setAccuracy(location.getAccuracy());
+
+            if (BuildConfig.DEBUG) {
+                Log.d(LT, "locationAfterUpdateStep: " + loc.getLongitude() + "," + loc.getLatitude());
+            }
+
+            return loc;
+        }
+
         @Override
-        public void onLocationChanged(Location location) {
+        public void onLocationChanged(Location originalLocation) {
             if (BuildConfig.DEBUG) {
                 Log.d(LT, "onLocationChanged");
             }
+            lastDataItemGPS = new DataItemGPS(originalLocation);
+            if (mKalmanFilter == null) {
+                Location location = originalLocation;
+                double x, y, xVel, yVel, posDev, course, speed;
+                long timeStamp;
+                speed = location.getSpeed();
+                course = location.getBearing();
+                x = location.getLongitude();
+                y = location.getLatitude();
+                xVel = speed * Math.cos(course);
+                yVel = speed * Math.sin(course);
+                posDev = location.getAccuracy();
+                timeStamp = Utils.nano2milli(location.getElapsedRealtimeNanos());
+                mKalmanFilter = new GPSAccKalmanFilter(
+                        false, //todo move to settings
+                        Coordinates.longitudeToMeters(x),
+                        Coordinates.latitudeToMeters(y),
+                        xVel,
+                        yVel,
+                        filterSettings.accelerationDeviation,
+                        posDev,
+                        timeStamp,
+                        filterSettings.mVelFactor,
+                        filterSettings.mPosFactor);
+                handlePredict(zero);
+            }
+            double ts = lastDataItemGPS.getTimestamp();
+            if (ts < lastGPSTimeStamp) {
+                return;
+            }
+            lastGPSTimeStamp = ts;
+
+            handleUpdate(lastDataItemGPS, lastDataItemGPS.location);
+            Location location = locationAfterUpdateStep(lastDataItemGPS.location);
+
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+
+            if (BuildConfig.DEBUG) {
+                Log.d(LT, "Request to publish location " + lon + "," + lat);
+            }
+
+            if (lat == 0 || lon == 0) {
+                return;
+            }
+
+            if (Math.abs(lat - prevLat) < minDistance && Math.abs(lon - prevLon) < minDistance) {
+                if (BuildConfig.DEBUG) {
+                    Log.d(LT, "Skip upload because too close");
+                }
+                return;
+            }
+
+            prevLon = lon;
+            prevLat = lat;
             notifyLocation(location);
         }
 
@@ -88,6 +220,8 @@ public class LocationsTracker {
         LocationsTracker.updater = updater;
         isUpdating = true;
         isSuspended = false;
+        minDistance = frequency < 5000 ? 0 : frequency < 15000 ? 0.0002f : 0.0005f;
+
         notifyStateChange();
         updater.requestLocationUpdates(locationListener, requestListener, frequency);
     }
@@ -100,6 +234,7 @@ public class LocationsTracker {
         if (BuildConfig.DEBUG) {
             Log.d(LT, "stop");
         }
+        mKalmanFilter = null;
         if (updater != null) {
             updater.cancelLocationUpdates(locationListener);
             updater = null;
